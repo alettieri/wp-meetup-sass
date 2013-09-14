d009d2b8e75fef6f64230769d5d824ee3fb8d0aa
alettieri <alettieri@gmail.com>
2013-09-14 14:27:09 -0700

    Moved js into assets

--- functions.php
+++ functions.php
@@ -85,16 +85,16 @@ add_action( 'widgets_init', 'ebwp_widgets_init' );
 function ebwp_scripts() {
 	wp_enqueue_style( 'ebwp-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'ebwp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'ebwp-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'ebwp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'ebwp-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'ebwp-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'ebwp-keyboard-image-navigation', get_template_directory_uri() . '/assets/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'ebwp_scripts' );
